345458d2832baa3653d3d66c5adafc82bcfd91da
Adam Dougal <adamdougal@microsoft.com>
2023-12-01 14:24:24 +0000

    Day 1 Part 2

--- day-01/main.py
+++ day-01/main.py
@@ -1,20 +1,46 @@
 
+numbers = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
 
 def get_first_digit(line):
+    string = ''
     for char in range(0, len(line)):
+        for number in numbers:
+            if number in string:
+                return numbers[number]
+
         if line[char].isdigit():
             return line[char]
+        else:
+            string += line[char]
 
 
 def get_last_digit(line):
+    string = ''
     for char in range(len(line)-1, -1, -1):
+        for number in numbers:
+            if number in string:
+                return numbers[number]
+            
         if line[char].isdigit():
             return line[char]
+        else:
+            string = line[char] + string
         
 
 def calibration_value(line):
     return int(get_first_digit(line) + get_last_digit(line))
-    
+
 
 def calibrate(file_path):
     with open(file_path) as file:
--- day-01/main_test.py
+++ day-01/main_test.py
@@ -7,19 +7,34 @@ class TestCalibration(unittest.TestCase):
         self.assertEqual(get_first_digit("2aaa1aa3aa5aaa"), '2')
         self.assertEqual(get_first_digit("aaaaaaaaaaaaa3"), '3')
         self.assertEqual(get_first_digit("4aaaaaaaaaaaaa"), '4')
+        self.assertEqual(get_first_digit("two1nine"), '2')
+        self.assertEqual(get_first_digit("eightwothree"), '8')
+        self.assertEqual(get_first_digit("abcone2threexyz"), '1')
+        self.assertEqual(get_first_digit("7pqrstsixteen"), '7')
 
     def test_get_last_digit(self):
         self.assertEqual(get_last_digit("aaaa1aa3aa5aaa"), '5')
         self.assertEqual(get_last_digit("aaaa1aa3aa5aa2"), '2')
         self.assertEqual(get_last_digit("aaaaaaaaaaaaa3"), '3')
         self.assertEqual(get_last_digit("4aaaaaaaaaaaaa"), '4')
+        self.assertEqual(get_last_digit("two1nine"), '9')
+        self.assertEqual(get_last_digit("eightwothree"), '3')
+        self.assertEqual(get_last_digit("abcone2threexyz"), '3')
+        self.assertEqual(get_last_digit("7pqrstsixteen"), '6')
 
     def test_calibration_value(self):
         self.assertEqual(calibration_value("aaaa1aa3aa5aaa"), 15)
         self.assertEqual(calibration_value("2aaa1aa3aa5aa2"), 22)
+        self.assertEqual(calibration_value("two1nine"), 29)
+        self.assertEqual(calibration_value("eightwothree"), 83)
+        self.assertEqual(calibration_value("abcone2threexyz"), 13)
+        self.assertEqual(calibration_value("7pqrstsixteen"), 76)
 
     def test_calibration(self):
         self.assertEqual(calibrate('test_input.txt'), 142)
+        self.assertEqual(calibrate('test_input_with_words.txt'), 281)
+
+
 
 
 if __name__ == '__main__':
